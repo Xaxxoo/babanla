b4d826714a35901e4ffd6bfac45ecc24f6ec835a
“Xaxxoo” <bnahmad83@gmail.com>
2023-10-01 15:59:53 +0100

    the whole app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-0">
+    <p className="dark:text-gray-200 bg:gray text-gray-700 text-center mb-4">
+      © 2023 All rights reserved by aliyu.dev
+    </p>
+  </div>
+);
+
+export default Footer;
